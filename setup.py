e67cebbc221ee84a9d71dbe8ae00f02e9f50bb32
Danyal Alam <danyalalam514@gmail.com>
2024-08-16 15:26:17 +0500

    installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Danyal",
+    author_email="danyal514@gmail.com"
+)
